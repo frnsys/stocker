4a71678faf6cd6c7bc9d1b2fbfa63dee262b4271
Francis Tseng <ftzeng@gmail.com>
2013-05-02 16:32:01 -0400

    added quote properties \– whew

--- stocker.py
+++ stocker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# With many thanks to http://goo.gl/gSEmw
 
 import sys
 import json
@@ -10,6 +11,95 @@ BASE_PARAMS = {
 		'env': 'store://datatables.org/alltableswithkeys'
 }
 
+QUOTE_PROPERTIES = {
+			'AfterHoursChangeRealtime': 'c8',
+			'AnnualizedGain': 'g3',
+			'Ask': 'a0',
+			'AskRealtime': 'b2',
+			'AskSize': 'a5',
+			'AverageDailyVolume': 'a2',
+			'Bid': 'b0',
+			'BidRealtime': 'b3',
+			'BidSize': 'b6',
+			'BookValuePerShare': 'b4',
+			'Change': 'c1',
+			'ChangeChangeInPercent': 'c0',
+			'ChangeFromFiftyDayMovingAverage': 'm7',
+			'ChangeFromTwoHundredDayMovingAverage': 'm5',
+			'ChangeFromYearHigh': 'k4',
+			'ChangeFromYearLow': 'j5',
+			'ChangeInPercent': 'p2',
+			'ChangeInPercentRealtime': 'k2',
+			'ChangeRealtime': 'c6',
+			'Commission': 'c3',
+			'Currency': 'c4',
+			'DaysHigh': 'h0',
+			'DaysLow': 'g0',
+			'DaysRange': 'm0',
+			'DaysRangeRealtime': 'm2',
+			'DaysValueChange': 'w1',
+			'DaysValueChangeRealtime': 'w4',
+			'DividendPayDate': 'r1',
+			'TrailingAnnualDividendYield': 'd0',
+			'TrailingAnnualDividendYieldInPercent': 'y0',
+			'DilutedEPS': 'e0',
+			'EBITDA': 'j4',
+			'EPSEstimateCurrentYear': 'e7',
+			'EPSEstimateNextQuarter': 'e9',
+			'EPSEstimateNextYear': 'e8',
+			'ExDividendDate': 'q0',
+			'FiftyDayMovingAverage': 'm3',
+			'SharesFloat': 'f6',
+			'HighLimit': 'l2',
+			'HoldingsGain': 'g4',
+			'HoldingsGainPercent': 'g1',
+			'HoldingsGainPercentRealtime': 'g5',
+			'HoldingsGainRealtime': 'g6',
+			'HoldingsValue': 'v1',
+			'HoldingsValueRealtime': 'v7',
+			'LastTradeDate': 'd1',
+			'LastTradePriceOnly': 'l1',
+			'LastTradeRealtimeWithTime': 'k1',
+			'LastTradeSize': 'k3',
+			'LastTradeTime': 't1',
+			'LastTradeWithTime': 'l0',
+			'LowLimit': 'l3',
+			'MarketCapitalization': 'j1',
+			'MarketCapRealtime': 'j3',
+			'MoreInfo': 'i0',
+			'Name': 'n0',
+			'Notes': 'n4',
+			'OneyrTargetPrice': 't8',
+			'Open': 'o0',
+			'OrderBookRealtime': 'i5',
+			'PEGRatio': 'r5',
+			'PERatio': 'r0',
+			'PERatioRealtime': 'r2',
+			'PercentChangeFromFiftyDayMovingAverage': 'm8',
+			'PercentChangeFromTwoHundredDayMovingAverage': 'm6',
+			'ChangeInPercentFromYearHigh': 'k5',
+			'PercentChangeFromYearLow': 'j6',
+			'PreviousClose': 'p0',
+			'PriceBook': 'p6',
+			'PriceEPSEstimateCurrentYear': 'r6',
+			'PriceEPSEstimateNextYear': 'r7',
+			'PricePaid': 'p1',
+			'PriceSales': 'p5',
+			'Revenue': 's6',
+			'SharesOwned': 's1',
+			'SharesOutstanding': 'j2',
+			'ShortRatio': 's7',
+			'StockExchange': 'x0',
+			'Symbol': 's0',
+			'TickerTrend': 't7',
+			'TradeDate': 'd2',
+			'TwoHundredDayMovingAverage': 'm4',
+			'Volume': 'v0',
+			'YearHigh': 'k0',
+			'YearLow': 'j0',
+			'YearRange': 'w0'
+		}
+
 
 def quotes( *symbols ):
 	'''
@@ -20,6 +110,13 @@ def quotes( *symbols ):
 	Returns:
 		Array of quotes
 	'''
+
+
+	params = {
+				# Get all data
+				'f': 'p0p1p2snhcgabmkjwrdyeqflvtiox'
+			}
+
 	query = BASE_PARAMS.copy()
 	s = ', '.join('"%s"' % i for i in symbols)
 	query['q'] = 'select * from yahoo.finance.quotes where symbol in (%s)' % s
