7a41f587983ebe263998f62b67a0d208025edf97
Francis Tseng <ftzeng@gmail.com>
2013-05-03 18:19:08 -0400

    industries, moved props to separate yaml

--- stocker.py
+++ stocker.py
@@ -2,116 +2,15 @@
 # With many thanks to http://goo.gl/gSEmw
 
 import sys
-import csv
+import os
 import requests
+import yaml
 from datetime import datetime
 
-# Thanks to http://goo.gl/wCyUQ
-QUOTE_PROPS = {
-			# Pricing
-			'ask': 'a',
-			'bid': 'b',
-			'ask_realtime': 'b2',
-			'bid_realtime': 'b3',
-			'previous_close': 'p',
-			'open': 'o',
-			'change': 'c1',
-			'change_and_percent_change': 'c',
-			'change_realtime': 'c6',
-			'change_percent_realtime': 'k2',
-			'change_in_percent': 'p2',
-			'after_hours_change_realtime': 'c8',
-			'commission': 'c3',
-			'days_low': 'g',
-			'days_high': 'h',
-			'last_trade_with_time_realtime': 'k1',
-			'last_trade_with_time': 'l',
-			'last_trade_price_only': 'l1',
-			'one_year_target_price': 't8',
-			'days_value_change': 'w1',
-			'days_value_change_realtime': 'w4',
-			'price_paid': 'p1',
-			'days_range': 'm',
-			'days_range_realtime': 'm2',
-
-			# 52 Week Pricing
-			'52_week_high': 'k',
-			'52_week_low': 'j',
-			'change_from_52_week_low': 'j5',
-			'change_from_52_week_high': 'k4',
-			'percent_change_from_52_week_low': 'j6',
-			'percent_Change_from_52_week_high': 'k5',
-			'52_week_range': 'w',
-
-			# Volume
-			'volume': 'v',
-			'ask_size': 'a5',
-			'bid_size': 'b6',
-			'last_trade_size': 'k3',
-			'average_daily_volume': 'a2',
-
-			# Ratios
-			'eps': 'e',
-			'eps_estimate_current_year': 'e7',
-			'eps_estimate_next_year': 'e8',
-			'eps_estimate_next_quarter': 'e9',
-			'book_value': 'b4',
-			'ebitda': 'j4',
-			'price_to_sales': 'p5',
-			'price_to_book': 'p6',
-			'pe_ratio': 'r',
-			'pe_ratio_realtime': 'r2',
-			'peg_ratio': 'r5',
-			'price_to_eps_estimate_current_year': 'r6',
-			'price_to_eps_estimate_next_year': 'r7',
-			'short_ratio': 's7',
-
-			# Dividends
-			'dividend_yield': 'y',
-			'dividend_per_share': 'd',
-			'dividend_pay_date': 'r1',
-			'ex_dividto_date': 'q',
-
-			# Date
-			'last_trade_date': 'd1',
-			'trade_date': 'd2',
-			'last_trade_time': 't1',
-
-			# Averages
-			'change_from_200_day_moving_average': 'm5',
-			'percent_change_from_200_day_moving_average': 'm6',
-			'change_from_50_day_moving_average': 'm7',
-			'percent_change_from_50_day_moving_average': 'm8',
-			'50_day_moving_average': 'm3',
-			'200_day_moving_average': 'm4',
-
-			# Misc
-			'holdings_gain_percent': 'g1',
-			'annualized_gain': 'g3',
-			'holdings_gain': 'g4',
-			'holdings_gain_percent_realtime': 'g5',
-			'holdings_gain_realtime': 'g6',
-
-			# Symbol Info
-			'more_info': 'v',
-			'market_cap': 'j1',
-			'market_cap_realtime': 'j3',
-			'float_shares': 'f6',
-			'name': 'n',
-			'notes': 'n4',
-			'symbol': 's',
-			'shares_owned': 's1',
-			'stock_exchange': 'x',
-
-			# Misc
-			'ticker_trend': 't7',
-			'trade_links': 't6',
-			'order_book_realtime': 'i5',
-			'high_limit': 'l2',
-			'low_limit': 'l3',
-			'holdings_value': 'v1',
-			'holdings_value_realtime': 'v7'
-		}
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+props = yaml.load(open(os.path.join(__location__, 'properties.yml')))
+QUOTE_PROPS = props['quotes']
+INDUSTRIES = props['industries']
 
 def quotes( symbols, props=[] ):
 	'''
@@ -193,13 +92,23 @@ def history( symbols, from_date, to_date, interval ):
 
 	return results
 
-def sector( sort='u' ):
+def sectors( sort='u' ):
 	api_url = 'http://biz.yahoo.com/p/csv/s_coname'
 
 	if sort not in ['u','d']: sort = 'u'
 
 	r = requests.get( api_url + sort + '.csv' )
 	print r.url
+	print r.text
+
+def industries( industry, sort='u' ):
+	api_url = 'http://biz.yahoo.com/p/csv/'
+
+	if industry not in INDUSTRIES: return
+	if sort not in ['u','d']: sort = 'u'
+
+	url = api_url + str(INDUSTRIES[industry]) + 'coname' + sort + '.csv'
+	r = requests.get( url )
 	print r.text
 	
 
@@ -210,7 +119,8 @@ def main():
 	args = sys.argv[1:]
 
 	#results = history( args, "3/15/2000", "1/31/2010", 'w' )
-	results = sector('name')
+	#results = sectors()
+	results = industries('services')
 
 	if not results:
 		sys.exit(1)
